94bb477ab8418a3940011fc3ce65f03872af2a4e
Dennis Heiden <dennis-heiden@straightvisions.com>
2020-06-15 15:22:17 +0200

    change default flex behaviour and grid margins for columns

--- lib/frontend/css/config.php
+++ lib/frontend/css/config.php
@@ -3,12 +3,19 @@
 
 	// maybe stack
 	$properties					= array();
+	
 	$stack = array_map(function ($val) {
-		return $val ? 'block' : 'flex';
+		return $val ? 'column' : 'row';
 	}, $stack_active->get_data());
 
-	$properties['display']	= $stack_active->prepare_css_property_responsive($stack,'','');
-
+	$properties['flex-direction']	= $stack_active->prepare_css_property_responsive($stack,'','');
+	
+	$stack = array_map(function ($val) {
+		return $val ? 'wrap' : 'nowrap';
+	}, $stack_active->get_data());
+	
+	$properties['flex-wrap']	= $stack_active->prepare_css_property_responsive($stack,'','');
+	
 	echo $_s->build_css(
 		is_admin() ? '.editor-styles-wrapper .wp-block-columns' : '.sv100_sv_content_wrapper article .wp-block-columns',
 		array_merge(
@@ -18,27 +25,32 @@
 			$script->get_parent()->get_setting('border')->get_css_data()
 		)
 	);
-
-
-	// maybe stack -> remove margin left when stacked
-	$properties					= array();
+	
 	$stack = array_map(function ($val) {
-		return $val ? '30px auto 0px auto !important' : '0px 0px 0px 32px !important';
+		return $val ? '20px' : '0';
 	}, $stack_active->get_data());
-
-	$properties['margin']	= $stack_active->prepare_css_property_responsive($stack,'','');
-
-	echo $stack_active->build_css(
-		is_admin() ? '.edit-post-visual-editor.editor-styles-wrapper .wp-block-columns .wp-block-column:not(:first-child)' : '.sv100_sv_content_wrapper article .wp-block-columns .wp-block-column:not(:first-child)',
-		$properties
+	
+	$properties['margin-top']	= $stack_active->prepare_css_property_responsive($stack,'','');
+	
+	$stack = array_map(function ($val) {
+		return $val ? '0' : '20px';
+	}, $stack_active->get_data());
+	
+	$properties['margin-left']	= $stack_active->prepare_css_property_responsive($stack,'','');
+	
+	echo $_s->build_css(
+		is_admin() ? '.editor-styles-wrapper .wp-block-columns > .wp-block-column:not(:first-child)'
+			: '.sv100_sv_content_wrapper article .wp-block-columns > .wp-block-column:not(:first-child)',
+		array_merge(
+			$properties
+		)
 	);
-
 
 	// maybe stack -> add max width text when stacked
 	$properties					= array();
 
 	$stack_max_width = array_map(function ($val) {
-		return $val ? 'var( --sv100_sv_common-max-width-alignwide ) !important' : 'inherit !important';
+		return $val ? 'var( --sv100_sv_common-max-width-alignwide )' : '100%';
 	}, $stack_active->get_data());
 
 	$properties['max-width']	= $stack_active->prepare_css_property_responsive($stack_max_width,'','');
