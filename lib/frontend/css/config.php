717ea72625d54c28e30e66504739c2a5d338a30f
Dennis Heiden <dennis-heiden@straightvisions.com>
2020-07-31 17:30:08 +0200

    change config.php generation - remove article + remove wp-block-column (managed else where now)

--- lib/frontend/css/config.php
+++ lib/frontend/css/config.php
@@ -17,7 +17,7 @@
 	$properties['flex-wrap']	= $stack_active->prepare_css_property_responsive($stack,'','');
 	
 	echo $_s->build_css(
-		is_admin() ? '.editor-styles-wrapper .wp-block-columns' : '.sv100_sv_content_wrapper article .wp-block-columns',
+		is_admin() ? '.editor-styles-wrapper .wp-block-columns' : '.sv100_sv_content_wrapper .wp-block-columns',
 		array_merge(
 			$properties,
 			$script->get_parent()->get_setting('padding')->get_css_data('padding'),
@@ -34,28 +34,7 @@
 	$properties['margin-top']	= $stack_active->prepare_css_property_responsive($stack,'','');
 	
 	echo $_s->build_css(
-		is_admin() ? '.editor-styles-wrapper .wp-block-columns+.wp-block-columns' : '.sv100_sv_content_wrapper article .wp-block-columns+.wp-block-columns',
-		array_merge(
-			$properties
-		)
-	);
-	
-	$properties					= array();
-	$stack = array_map(function ($val) {
-		return $val ? '20px' : '0';
-	}, $stack_active->get_data());
-	
-	$properties['margin-top']	= $stack_active->prepare_css_property_responsive($stack,'','');
-	
-	$stack = array_map(function ($val) {
-		return $val ? '0' : '20px';
-	}, $stack_active->get_data());
-	
-	$properties['margin-left']	= $stack_active->prepare_css_property_responsive($stack,'','');
-	
-	echo $_s->build_css(
-		is_admin() ? '.editor-styles-wrapper .wp-block-columns > .wp-block-column:not(:first-child)'
-			: '.sv100_sv_content_wrapper article .wp-block-columns > .wp-block-column:not(:first-child)',
+		is_admin() ? '.editor-styles-wrapper .wp-block-columns+.wp-block-columns' : '.sv100_sv_content_wrapper .wp-block-columns+.wp-block-columns',
 		array_merge(
 			$properties
 		)
@@ -71,6 +50,6 @@
 	$properties['max-width']	= $stack_active->prepare_css_property_responsive($stack_max_width,'','');
 
 	echo $stack_active->build_css(
-		is_admin() ? '.edit-post-visual-editor.editor-styles-wrapper .wp-block-columns .wp-block-column' : '.sv100_sv_content_wrapper article .wp-block-columns .wp-block-column',
+		is_admin() ? '.edit-post-visual-editor.editor-styles-wrapper .wp-block-columns .wp-block-column' : '.sv100_sv_content_wrapper .wp-block-columns .wp-block-column',
 		$properties
 	);
