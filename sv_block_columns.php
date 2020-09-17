5eaceab22e64f80781a911beed7a34512608c414
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-09-17 21:23:54 +0200

    CENTRALIZE ENQUEUE SCRIPTS

--- sv_block_columns.php
+++ sv_block_columns.php
@@ -112,9 +112,6 @@
 				->set_path( 'lib/frontend/css/config.php' )
 				->set_inline( true );
 
-			add_action('wp', array($this,'enqueue_scripts'));
-			add_action('admin_init', array($this,'enqueue_scripts'));
-
 			return $this;
 		}
 		public function enqueue_scripts(): sv_block_columns {
