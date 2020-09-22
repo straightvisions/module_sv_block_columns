24e24a2d2c313009bcdda1c61b80618ce2e22ef0
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-09-22 20:38:42 +0200

    FIX TEMPLATE PATH

--- lib/tpl/settings/init.php
+++ lib/tpl/settings/init.php
@@ -4,7 +4,7 @@
 		<ul class="sv_setting_subpages_nav"></ul>
 		<?php
 			require_once( $module->get_path( 'lib/tpl/settings/general.php' ) );
-			require_once( $module->get_path( 'lib/backend/tpl/column.php' ) );
+			require_once( $module->get_path( 'lib/tpl/settings/column.php' ) );
 		?>
 	</div>
 	<?php
