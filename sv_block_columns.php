e12b059d2fbd7d730f4a3ffbd06b8f1f814a080c
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-12-06 14:30:11 +0100

    UPDATE ICON

--- sv_block_columns.php
+++ sv_block_columns.php
@@ -8,9 +8,9 @@
 				->set_css_cache_active()
 				->set_section_title( $this->get_module_title() )
 				->set_section_desc( $this->get_module_desc() )
-				->set_section_type( 'settings' )
 				->set_section_template_path()
 				->set_section_order(5000)
+				->set_section_icon('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M6 24h-6v-24h6v24zm9-24h-6v24h6v-24zm9 0h-6v24h6v-24z"/></svg>')
 				->get_root()
 				->add_section( $this );
 		}
