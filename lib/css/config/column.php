941fd0fb16fcde65d7f58e6e32a76efb4fd81b66
Matthias Bathke <matthias-bathke@straightvisions.com>
2021-03-11 14:31:59 +0100

    ADD MARGIN LEFT 0 FOR COLUMN WHEN STACKED

--- lib/css/config/column.php
+++ lib/css/config/column.php
@@ -1,4 +1,18 @@
 <?php
+	$stack_active				= $module->get_setting('stack_active');
+	$stack = array_map(function ($val) {
+		return boolval($val) ? '0' : '';
+	}, $stack_active->get_data());
+
+	$properties['margin-left']	= $stack_active->prepare_css_property_responsive($stack,'','');
+
+	echo $_s->build_css(
+		is_admin() ? '.editor-styles-wrapper .wp-block-columns .wp-block-column' : '.sv100_sv_content_wrapper .wp-block-columns .wp-block-column',
+		array_merge(
+			$properties
+		)
+	);
+
 	echo $_s->build_css(
 		is_admin() ? '.editor-styles-wrapper .wp-block-columns .wp-block-column' : '.sv100_sv_content_wrapper .wp-block-columns .wp-block-column',
 		array_merge(
