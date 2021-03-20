a406c60fa45eec6cb1328327ef150b53023e0d02
Matthias Bathke <matthias-bathke@straightvisions.com>
2021-03-20 18:12:58 +0100

    FIX MARGIN LEFT CALCULATION FOR RESPONSIVE STACKS

--- lib/css/config/column.php
+++ lib/css/config/column.php
@@ -1,8 +1,26 @@
 <?php
+	echo $_s->build_css(
+		is_admin() ? '.editor-styles-wrapper .wp-block-columns .wp-block-column' : '.sv100_sv_content_wrapper .wp-block-columns .wp-block-column',
+		array_merge(
+			$module->get_setting('single_padding')->get_css_data('padding'),
+			$module->get_setting('single_margin')->get_css_data(),
+			$module->get_setting('single_border')->get_css_data()
+		)
+	);
+
+	$properties					= array();
+
 	$stack_active				= $module->get_setting('stack_active');
 	$stack = array_map(function ($val) {
-		return boolval($val) ? '0' : '';
+		return boolval($val) ? '0' : 'invalid';
 	}, $stack_active->get_data());
+
+	$margin_left_data			= $module->get_setting('single_margin')->get_data();
+	foreach($stack as $responsive => $val){
+		if($val === 'invalid'){
+			$stack[$responsive]		= $margin_left_data[$responsive]['left'];
+		}
+	}
 
 	$properties['margin-left']	= $stack_active->prepare_css_property_responsive($stack,'','');
 
@@ -10,14 +28,5 @@
 		is_admin() ? '.editor-styles-wrapper .wp-block-columns .wp-block-column' : '.sv100_sv_content_wrapper .wp-block-columns .wp-block-column',
 		array_merge(
 			$properties
-		)
-	);
-
-	echo $_s->build_css(
-		is_admin() ? '.editor-styles-wrapper .wp-block-columns .wp-block-column' : '.sv100_sv_content_wrapper .wp-block-columns .wp-block-column',
-		array_merge(
-			$module->get_setting('single_padding')->get_css_data('padding'),
-			$module->get_setting('single_margin')->get_css_data(),
-			$module->get_setting('single_border')->get_css_data()
 		)
 	);
