f2f574a49dd1c3720a1e14e5dd67e52083799d3c
Dennis Heiden <dennis-heiden@straightvisions.com>
2021-08-09 14:55:52 +0200

    remove margin from columns css generator

--- lib/css/config/general.php
+++ lib/css/config/general.php
@@ -21,7 +21,6 @@
 		array_merge(
 			$properties,
 			$module->get_setting('padding')->get_css_data('padding'),
-			$module->get_setting('margin')->get_css_data(),
 			$module->get_setting('border')->get_css_data()
 		)
 	);
