b13c12d01bc7560110f746cb3853926d4e7c1ad7
Dennis Heiden <dennis-heiden@straightvisions.com>
2021-08-10 11:16:53 +0200

    remove single margin settings from registry

--- sv_block_columns.php
+++ sv_block_columns.php
@@ -52,17 +52,6 @@
 				->set_is_responsive(true)
 				->load_type( 'border' );
 
-			$this->get_setting( 'single_margin' )
-				->set_title( __( 'Margin', 'sv100' ) )
-				->set_default_value(array(
-					'top'		=> '0',
-					'right'		=> 'auto',
-					'bottom'	=> '32px',
-					'left'		=> 'auto'
-				))
-				->set_is_responsive(true)
-				->load_type( 'margin' );
-
 			$this->get_setting( 'single_padding' )
 				->set_title( __( 'Padding', 'sv100' ) )
 				->set_is_responsive(true)
