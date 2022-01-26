26bd8c16b34f71da25f02afe74996f3b1d39bfe5
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-01-26 19:22:19 +0100

    ADD - SUPPORT - .is-not-stacked-on-mobile CLASS FIX - IGNORE flex-basis when stacked

--- lib/css/config/general.php
+++ lib/css/config/general.php
@@ -15,7 +15,7 @@
 	}, $stack_active->get_data());
 	
 	$properties['flex-wrap']	= $stack_active->prepare_css_property_responsive($stack,'','');
-	
+
 	echo $_s->build_css(
 		is_admin() ? '.editor-styles-wrapper .wp-block-columns' : '.wp-block-columns',
 		array_merge(
@@ -23,5 +23,22 @@
 			$module->get_setting('padding')->get_css_data('padding'),
 			$module->get_setting('margin')->get_css_data(),
 			$module->get_setting('border')->get_css_data()
+		)
+	);
+
+
+	// If columns are stacked, ignore flex-basis to avoid using flex-basis for columns height
+	$properties					= array();
+
+	$stack = array_map(function ($val) {
+		return $val ? 'auto !important' : 'initial';
+	}, $stack_active->get_data());
+
+	$properties['flex-basis']	= $stack_active->prepare_css_property_responsive($stack,'','');
+
+	echo $_s->build_css(
+		is_admin() ? '.editor-styles-wrapper .wp-block-columns .wp-block-column' : '.wp-block-columns .wp-block-column',
+		array_merge(
+			$properties
 		)
 	);
