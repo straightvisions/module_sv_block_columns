e8f5064c6cadbcd97ceea99a7a8dd45d82109492
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-07-20 14:39:52 +0200

    support for default non breaking mobile property

--- lib/css/config/general.php
+++ lib/css/config/general.php
@@ -15,17 +15,17 @@
 	}, $stack_active->get_data());
 
 	$properties['display']	= $stack_active->prepare_css_property_responsive($stack,'','');
-	/*
-	$stack = array_map(function ($val) {
-		return $val ? 'wrap' : 'nowrap';
-	}, $stack_active->get_data());
-	
-	$properties['flex-wrap']	= $stack_active->prepare_css_property_responsive($stack,'','');
-*/
+
+	echo $_s->build_css(
+		'div.wp-block-columns:not(.is-not-stacked-on-mobile)',
+		array_merge(
+			$properties
+		)
+	);
+
 	echo $_s->build_css(
 		is_admin() ? '.editor-styles-wrapper .wp-block-columns' : 'div.wp-block-columns',
 		array_merge(
-			$properties,
 			$module->get_setting('padding')->get_css_data('padding'),
 			$module->get_setting('margin')->get_css_data(),
 			$module->get_setting('border')->get_css_data()
